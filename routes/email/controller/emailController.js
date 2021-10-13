5f879898538522b2ad14c628d317e06fa6e13958
Nastya Kvak <nastya.kvak@codeimmersives.com>
2021-10-13 13:04:00 -0400

    AWS

--- routes/email/controller/emailController.js
+++ routes/email/controller/emailController.js
@@ -5,24 +5,23 @@ CLIENT_ID="22212146535-lfca68h6mag0ssvi14g9qadf2cj5i9bo.apps.googleusercontent.c
 CLIENT_SECRET="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"
 REDIRECT_URI="https://developers.google.com/oauthplayground"
 REFRESH_TOKEN="1//04ZsGBvRvTjv5CgYIARAAGAQSNwF-L9IrT-s2RnSMozshxgxFOXRoCran4Au3uakvmq2NG0h62NiOuOYzhHo7WKQyXmjSehBORus"
-const OAuth2 = google.auth.OAuth2
-const oAuth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET)
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN})
-function transport (){
-  const accessToken = oAuth2Client.getAccessToken()
-  var transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    service:"gmail",
-    auth: {
-      type: 'OAuth2',
-      user: process.env.email,
-      clientID: CLIENT_ID,
-      clientSecret:CLIENT_SECRET,
-      refreshToken: REFRESH_TOKEN,
-      accessToken: accessToken
-      }
-    });
-}
+const accessToken = oAuth2Client.getAccessToken()
+
+var transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  service:"gmail",
+  auth: {
+    type: 'OAuth2',
+    user: process.env.email,
+    clientID: CLIENT_ID,
+    clientSecret:CLIENT_SECRET,
+    refreshToken: REFRESH_TOKEN,
+    accessToken: accessToken
+    }
+  });
 
 
-module.exports = transport;
+module.exports = transporter;
